a7e677b7f0eb3eb6c28fcb3b36a15b3ec32bc03c
bparrill <bparrill+gh@gmail.com>
2020-04-23 13:47:00 -0500

    Update chapter 7

--- DO180/ch7/multicontainer-design/deploy/nodejs/build.sh
+++ DO180/ch7/multicontainer-design/deploy/nodejs/build.sh
@@ -4,10 +4,6 @@ rm -fr build
 mkdir -p build
 cp -ap nodejs-source/* build
 rm build/*.sh
-#cp -p db.js build/models
-chmod -R a+rwX build
-
-# base image ONBUILD does npm install
 
 # image build complains if he cannot read the database folder even if not needed for building the image
 sudo rm -rf {linked,kubernetes}/work
